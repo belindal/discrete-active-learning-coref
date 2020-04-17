a6c945d3adb3cfaf4e5d628d981e20c886b66833
Gabi Satanovsky <gabriel.satanovsky@gmail.com>
2020-04-16 17:15:56 -0700

    error analysis scripts

--- error_analysis/analyze.py
+++ error_analysis/analyze.py
@@ -14,16 +14,28 @@ import numpy as np
 from collections import defaultdict
 from glob import glob
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from allennlp.models.coreference_resolution.coref import ConllCorefScores
+import spacy
+from spacy.tokenizer import Tokenizer
+
+plt.rcParams.update({'font.size': 14})
+plt.rc('xtick', labelsize=12)
+plt.rc('ytick', labelsize=12)
 
 # Local imports
 
 #----
 
+# global spacy instance
+nlp = spacy.load("en_core_web_sm", disable = ["parser", "ner"])
+nlp.tokenizer = Tokenizer(nlp.vocab) # tokenize using whitespace
+
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 f1 = lambda p, r: ((2*p*r) / (p + r)) if (p + r) else 0
-pr = lambda intr, pred: (intr / pred) if pred else 1
-rc = lambda intr, gold: (intr / gold) if gold else 1
+pr = lambda intr, pred: 100 * (intr / pred) if pred else 100
+rc = lambda intr, gold: 100 * (intr / gold) if gold else 100
 
 # conersions from (annotations / doc) to (min / doc)
 TIME_CONVERSIONS = {0: 0.0, 200: 71.91751439779348, 100: 41.96964765646017, 80: 34.27887468868257, 20: 9.38844897854418}
@@ -37,7 +49,7 @@ def get_entities(doc, clusters):
     for clust in clusters:
         ent_clust = []
         for (s, e) in clust:
-            ent_clust.append(" ".join(doc[s : e + 1]))
+            ent_clust.append(doc[s : e + 1])
         ent_clusts.append(ent_clust)
     return ent_clusts
 
@@ -78,6 +90,15 @@ def macro_f1(pred_cnts, gold_cnts, intr_cnts):
                  "r": macro_r}
     return ret_dict
 
+def contains_pronoun(cluster):
+    """
+    Returns True iff the given cluster contains a pronoun.
+    """
+    for mention in cluster:
+        if any([w.tag_.startswith("PRP") for w in mention]):
+            # Found a mention with a pronoun
+            return True
+    return False
 
 def get_stats(inp_fn):
     """
@@ -86,13 +107,30 @@ def get_stats(inp_fn):
     ent_pred_cnts = []
     ent_gold_cnts = []
     ent_intr_cnts = []
+    ccs = ConllCorefScores()
     jsons = [json.loads(line) for line in open(inp_fn, encoding = "utf8")]
     for doc_dict in jsons:
-        cur_doc = doc_dict["document"]
-        gold_clusts = get_entities(cur_doc, doc_dict["gold_clusters"])
-        pred_clusts = get_entities(cur_doc, doc_dict["clusters"])
-        pred_ents = set(flatten(pred_clusts))
-        gold_ents = set(flatten(gold_clusts))
+        cur_doc = nlp(" ".join(doc_dict["document"]))
+        gold_clusts = doc_dict["gold_clusters"]
+        pred_clusts = doc_dict["clusters"]
+
+        gold_ent_clusts = get_entities(cur_doc, gold_clusts)
+        pred_ent_clusts = get_entities(cur_doc, pred_clusts)
+
+        gold_clusts_pronouns = [gold_clust
+                                for gold_clust, gold_ent_clust
+                                in zip(gold_clusts, gold_ent_clusts)
+                                if contains_pronoun(gold_ent_clust)]
+
+        pred_clusts_pronouns = [pred_clust
+                                for pred_clust, pred_ent_clust
+                                in zip(pred_clusts, pred_ent_clusts)
+                                if contains_pronoun(pred_ent_clust)]
+
+        update_avg_f1(ccs, gold_clusts_pronouns, pred_clusts_pronouns)
+
+        pred_ents = set(map(str, flatten(pred_ent_clusts)))
+        gold_ents = set(map(str, flatten(gold_ent_clusts)))
         if (not pred_ents) and (not gold_ents):
             #TODO: what's going on in these instances?
             continue
@@ -103,7 +141,9 @@ def get_stats(inp_fn):
 
     ent_micro = micro_f1(ent_pred_cnts, ent_gold_cnts, ent_intr_cnts)
     ent_macro = macro_f1(ent_pred_cnts, ent_gold_cnts, ent_intr_cnts)
-    ret_dict = {"ent_micro": ent_micro, "ent_macro": ent_macro}
+    ret_dict = {"ent_micro": ent_micro,
+                "ent_macro": ent_macro,
+                "pronoun_avg_f1": get_avg_f1(ccs)}
 
     logging.debug(f"micro = {ent_micro['f1']:.2f}, macro = {ent_macro['f1']:.2f}")
 
@@ -130,20 +170,52 @@ def plot_stats(model_dicts, out_fn, x_title, y_title, *keys):
         plot_data[model_name_str].append(ys)
 
     # Plot
-    fig, ax1 = plt.subplots()
+    fig, ax = plt.subplots()
     for model_name, model_data in plot_data.items():
         xs, ys = model_data
 
         # Convert to min / doc
         xs = [TIME_CONVERSIONS[x] for x in xs]
 
-        ax1.plot(xs, ys, label = model_name)
-        ax1.scatter(xs, ys)
+        ax.plot(xs, ys, label = model_name)
+        ax.scatter(xs, ys)
 
-    ax1.set_xlabel(x_title)
-    ax1.set_ylabel(y_title)
-    fig.legend()
+    # Fix plot formatting
+    s, e = ax.get_ylim()
+    # ax.yaxis.set_ticks(np.arange(s, e + 1, 1))
+    # ax.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.0f'))
+    ax.set_xlabel(x_title)
+    ax.set_ylabel(y_title)
+    fig.legend(loc = 'lower right', bbox_to_anchor=(0.9, 0.1))
     fig.savefig(out_fn)
+
+
+def update_avg_f1(ccs, gold_clusters, pred_clusters):
+    """
+    update our average f1 for the predicted vs. gold clusters.
+    """
+    gold_clusters, mention_to_gold = ccs.get_gold_clusters(gold_clusters)
+    pred_clusters, mention_to_pred = ccs.get_gold_clusters(pred_clusters)
+
+    # update scores
+    scorers = ccs.scorers
+
+    for scorer in scorers:
+        scorer.update(pred_clusters, gold_clusters,
+                      mention_to_pred, mention_to_gold)
+
+def get_avg_f1(ccs):
+    """
+    get our average f1 for the predicted vs. gold clusters.
+    """
+    scorers = ccs.scorers
+    f1_scores = []
+    for scorer in scorers:
+        f1_scores.append(scorer.get_f1())
+
+    avg_f1 = np.average(f1_scores)
+
+    return avg_f1
 
 if __name__ == "__main__":
     # Parse command line arguments
@@ -159,6 +231,7 @@ if __name__ == "__main__":
         logging.basicConfig(level = logging.INFO)
 
     # Start computation
+
     model_dicts = defaultdict(dict)
     for model_dir in tqdm(inp_fns):
         logging.info(f"Analyzing {model_dir}")
@@ -177,6 +250,12 @@ if __name__ == "__main__":
                out_fn / "mention_micro.png",
                "Annotation time [mins / doc]", "Mention detection [micro f1]",
                "ent_micro", "f1")
+
+    plot_stats(model_dicts,
+               out_fn / "pronoun_avg_f1.png",
+               "Annotation time [mins / doc]", "Pronoun clusters [avg f1]",
+               "pronoun_avg_f1")
+
 
     # End
     logging.info("DONE")
