1df8f039270b5f2d79dfee94c0bb64d33e931e5a
belindali <26785408+belindal@users.noreply.github.com>
2020-04-21 18:23:03 -0700

    Graphing scripts

--- graphing_scripts/deltas_script.py
+++ graphing_scripts/deltas_script.py
@@ -2,17 +2,25 @@ import json
 import glob
 import matplotlib.pyplot as plt
 import pdb
+import os
 
-discrete_dir = ["allennlp/discrete_entropy/", "allennlp/pairwise_entropy/"]
+PAIRWISE_Q_TIME = 15.961803738317756
+DISCRETE_Q_TIME = 15.573082474226803
+DISCRETE_ONLY_Q_TIME = 28.009875
+RESULTS_DIR = "/Users/belindali/allennlp"
+
+discrete_dir = ["discrete_entropy", "pairwise_entropy"]
 avg_deltas = []
 
 for selector_fn in discrete_dir:
-    pair_or_not = selector_fn[len("allennlp/"):len(selector_fn)-len("_entropy/")]
+    pair_or_not = selector_fn[:len(selector_fn)-len("_entropy")]
     selector = 'entropy'
     deltas = {}
-    for filename in glob.glob("allennlp/discrete_entropy/*_query_info.json"):
+    for filename in glob.glob(os.path.join(RESULTS_DIR, "discrete_entropy/*_query_info.json")):
         with open(filename) as info_file:
-            discrete_labels_per_doc = int(filename[len(selector_fn):len(filename)-len("_query_info.json")])
+            discrete_labels_per_doc = int(filename[len(
+                os.path.join(RESULTS_DIR, "discrete_entropy") + "/"
+            ):len(filename)-len("_query_info.json")])
             labels_queried = 0
             discrete_answers = 0
             query_info = json.load(info_file)
@@ -20,9 +28,12 @@ for selector_fn in discrete_dir:
                 labels_queried += query_info[doc]["num_queried"]
                 discrete_answers += query_info[doc]["not coref"]
             if discrete_labels_per_doc == 20 and selector == 'entropy':
-                print("queried: " + str((labels_queried * 10.75954115554078 + discrete_answers * 24.291006100177764)))
-            with open(selector_fn + str(discrete_labels_per_doc) + "_deltas.json") as f:
-                deltas[(labels_queried * 10.75954115554078 + discrete_answers * 24.291006100177764) / 3600] = \
+                print("queried: " + str((labels_queried * PAIRWISE_Q_TIME + discrete_answers * DISCRETE_Q_TIME)))
+            with open(os.path.join(
+                os.path.join(RESULTS_DIR, selector_fn),
+                str(discrete_labels_per_doc) + "_deltas.json"
+            )) as f:
+                deltas[(labels_queried * PAIRWISE_Q_TIME + discrete_answers * DISCRETE_Q_TIME) / 60 / 2102] = \
                     float(f.read().strip())
 
     print(pair_or_not + " " + selector)
@@ -37,7 +48,7 @@ for selector_fn in discrete_dir:
 print("")
 print(avg_deltas[0] / avg_deltas[1])
 
-plt.xlabel("Total annotation time (hrs)")
+plt.xlabel("Total annotation time (mins / doc)")
 plt.ylabel("Average ΔF1")
 plt.legend()
 plt.show()
--- graphing_scripts/time_normalized_graphing_script.py
+++ graphing_scripts/time_normalized_graphing_script.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import pdb
 import os
 
-PAIRWISE_Q_TIME = 15.312435 #16.829913253012048
-DISCRETE_Q_TIME = 17.2381266 #14.887502183406114
+PAIRWISE_Q_TIME = 15.961803738317756
+DISCRETE_Q_TIME = 15.573082474226803
+DISCRETE_ONLY_Q_TIME = 28.009875
 RESULTS_DIR = "/Users/belindali/allennlp"
 GRAPH_TIME_CUTOFF = 75
 
@@ -51,7 +52,7 @@ for selector_fn in discrete_dir:
                         discrete_answers = 183732
                 model_score = json.load(f)[metric]
                 discrete_time = (labels_queried * PAIRWISE_Q_TIME + discrete_answers * DISCRETE_Q_TIME) / 60 / 2102
-                only_discrete_time = labels_queried * (PAIRWISE_Q_TIME + DISCRETE_Q_TIME) / 60 / 2102
+                only_discrete_time = labels_queried * DISCRETE_ONLY_Q_TIME / 60 / 2102
                 if discrete_time < GRAPH_TIME_CUTOFF:
                     discrete_results[discrete_time] = model_score
                 if only_discrete_time < GRAPH_TIME_CUTOFF:
@@ -79,28 +80,21 @@ for selector_fn in discrete_dir:
 for selector_fn in discrete_dir_2:
     selector = selector_fn[len("discrete_"):len(selector_fn)]
     selector_fn = os.path.join(RESULTS_DIR, selector_fn)
-    discrete_results_lower = {}
-    discrete_results_upper = {}
+    discrete_results = {}
     for filename in glob.glob(os.path.join(selector_fn, "*.json")):
         with open(filename) as info_file:
             discrete_labels_per_doc = filename[len(selector_fn+"/"):len(filename)-len(".json")]
             labels_queried = discrete_dir_2_map[discrete_labels_per_doc]
             with open(os.path.join(selector_fn, str(discrete_labels_per_doc) + ".json")) as f:
                 metric = "best_validation_coref_f1"
-                time_spent_lower = (labels_queried * (DISCRETE_Q_TIME + PAIRWISE_Q_TIME)) / 60 / 2102
-                time_spent_upper = (labels_queried * DISCRETE_Q_TIME) / 60 / 2102
+                time_spent = DISCRETE_ONLY_Q_TIME * labels_queried / 60 / 2102
                 value = json.load(f)[metric]
-                if time_spent_lower < 70:
-                    discrete_results_lower[time_spent_lower] = value 
-                if time_spent_upper < 70:
-                    discrete_results_upper[time_spent_upper] = value
+                if time_spent < 70:
+                    discrete_results[time_spent] = value 
 
     print("discrete random 2")
     print(discrete_results)
-    #plt.plot(*zip(*sorted(discrete_results_lower.items())), '--', label="discrete (" + selector[:len(selector)-1] +
-    #                                                              ", fully labelled)", marker='o',
-    #         color='k', alpha=0.5)
-    plt.plot(*zip(*sorted(discrete_results_upper.items())), '--', label="discrete (" + selector[:len(selector)-1] +
+    plt.plot(*zip(*sorted(discrete_results.items())), '--', label="discrete (" + selector[:len(selector)-1] +
                                                                   ", fully labelled)", marker='o',
              color='k', alpha=0.5)
 
